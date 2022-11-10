c8a72a9aba64efe16f41861db94b4a5336423642
Meg Owens <megowens@Megs-MacBook-Air.local>
2022-11-10 13:57:51 -0500

    first edits

--- src/main/java/edu/virginia/cs/gui/WordleApplication.java
+++ src/main/java/edu/virginia/cs/gui/WordleApplication.java
@@ -3,16 +3,23 @@ package edu.virginia.cs.gui;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class WordleApplication extends Application {
+    GridPane root;
+    TextField L1;
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(WordleApplication.class.getResource("wordle-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("WORDLE");
+        Scene scene = new Scene(fxmlLoader.load(), 400, 500);
+        //scene.setFill(Color.BLACK);
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/edu/virginia/cs/gui/WordleController.java
+++ src/main/java/edu/virginia/cs/gui/WordleController.java
@@ -1,14 +1,98 @@
 package edu.virginia.cs.gui;
 
+import edu.virginia.cs.wordle.*;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
 
 public class WordleController {
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Wordle wordle = new WordleImplementation();
+    @FXML
+    private GridPane grid = new GridPane();
+
+    @FXML
+    Label errorLabel = new Label();
+    @FXML
+    private TextField enteredWord;
+    @FXML
+    private TextField letterEntered;
+    @FXML
+    private Label gameResult;
+
+    protected LetterResult[] guessResult;
+    Label l1 = new Label(), l2 = new Label(), l3 = new Label(), l4 = new Label(), l5 = new Label();
+    int r = 0;
+    public void initialize(){
+        grid.setBackground(new Background(new BackgroundFill(Color.GREY, CornerRadii.EMPTY, Insets.EMPTY)));
+        grid.setHgap(10.0);
+        grid.setVgap(5.0);
+        gameResult = new Label();
+        gameResult.setText("");
+        //enteredWord.setVisible(false);
     }
+
+    @FXML
+    protected void onTextEntryEnter(KeyEvent event) {
+        int colind = 0;
+        Label newChar = new Label();
+        newChar.setText(enteredWord.getText());
+        grid.add(newChar, colind, r);
+
+        if(wordle.getRemainingGuesses()<1) {
+            gameResult.setText("no guesses remaining. Answer was: " + wordle.getAnswer());
+        }
+        else if(event.getCode().equals(KeyCode.ENTER)){
+            try {
+                enteredWord.setText(enteredWord.getText().strip());
+                guessResult = wordle.submitGuess(enteredWord.getText());
+                errorLabel.setText("");
+
+                for(int i = 0; i < 5; i++) {
+                    Label letterToAdd = new Label();
+                    letterToAdd.setText(String.valueOf(enteredWord.getText().charAt(i)));
+                    if(guessResult.equals(LetterResult.YELLOW)){
+                        grid.setBackground();
+                    }
+                    grid.add(letterToAdd, i+1, r);
+                }
+
+                enteredWord.setText("");
+                r++;
+                /*
+                l1.setText(String.valueOf(enteredWord.getText().charAt(0)));
+                l2.setText(String.valueOf(enteredWord.getText().charAt(1)));
+                l3.setText(String.valueOf(enteredWord.getText().charAt(2)));
+                l4.setText(String.valueOf(enteredWord.getText().charAt(3)));
+                l5.setText(String.valueOf(enteredWord.getText().charAt(4)));
+                 */
+
+            }catch(IllegalWordException e) {
+                errorLabel.setText("word is invalid: enter a valid 5 letter word");
+                enteredWord.setText("");
+            }
+        }
+    }
+
+
+
+
+    /*
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("change y/n");
+    }
+     */
+
 }
